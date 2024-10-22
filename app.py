cca7205640dd77b3ae1d39f778db968db5497db5
aseilhamid <aseilhamid@gmail.com>
2024-10-22 11:06:43 +0300

    modification on app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from werkzeug.utils import quote
+from werkzeug.urls import quote
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, Flask!"
+def hello_world():
+    return 'Hello, World!, THis is python flask app using DevOps Tools'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
